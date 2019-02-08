a6f62096e0755bf1a520e0fe4cc1006a134959b2
Yaroslav <ykolodiy17@gmail.com>
2019-02-08 21:11:55 +0000

    some ex

---
+++ operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int a =11;
+  int b=43;
+
+  int sum=a+b;
+  int sub=a-b;
+  int mul=a*b;
+  int division=a/b;
+
+  printf("%d\n", sum);
+  printf("%d\n", sub);
+  printf("%d\n", mul);
+  printf("%d\n", division);
+
+  return 0;
+}
