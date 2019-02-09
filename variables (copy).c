d3f80f22ccb9284878ca7a2d48c0f337b7268f55
YK0L0DIY <ykolodiy17@gmail.com>
2019-02-09 21:43:27 +0000

    test

---
+++ variables (copy).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+        int a= 1;
+        float b=2.444;
+        double c= 123.434;
+
+        printf("a is an integer = %d\n", a);
+        printf("b is an float = %f\n", b);
+        printf("c is an double = %f\n", c);
+
+        return 0;
+}
