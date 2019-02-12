08cfb16185e675e06641e0e29c6ba97f406c1e3f
YK0L0DIY <ykolodiy17@gmail.com>
2019-02-12 08:45:27 +0000

    more exercices

--- perfect.c
+++ perfect.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+// the number is perfect if the sum of all of the dividors is equal to numbers
+// so this funcion
+// retun 1 if the nmber is perfect and 0 if the number is normal
 int perfect(int number){
+
         int sum=0;
-        // for(int i=0; i<number; i++) {
-        //         if (number%i==0) {
-        //                 sum+=i;
-        //         }
-        //         printf("%d\n", sum);
-        // }
+        float rest;
+        for(int i=1; i<number; i++) {
+                rest = number%i;
+                if (rest==0) {
+                        sum+=i;
+                }
+        }
+
+        if(sum==number) {
+                return 1;
+        }
         return 0;
-
-
 }
 
-
 int main(){
+
         int number;
         char cont[]="n";
         do {
                 printf("\n%s","Your number:" );
                 scanf("%d", &number);
+                int perf=perfect(number);
 
-                if (perfect(number)) {
+                if (perf) {
                         printf("%s\n", "Perfect number");
                 }else{
                         printf("%s\n", "Normal number");
---
+++ print1-20sameline.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+        for (int i =1; i<=20; i++) {
+                if(i==20){
+                        printf("%d\n", i);
+                }else{
+                        printf("%d ", i);
+                }
+        }
+}
