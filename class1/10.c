4c5b9cd776e6b09c78f4a7f7e8eeb24bcbc5d8f7
YK0L0DIY <ykolodiy17@gmail.com>
2019-02-14 12:08:23 +0000

    mais 1 ex

--- class1/10.c
+++ class1/10.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 
-int mian(void){
-  
+int main(void){
+  int resultado;
+        for (int j =555; j<=777; j++) {
+          resultado=0;
+                for (int i = 1; i <= j / 2; i++) {
+                        if (j % i == 0) {
+                                resultado++;
+                        }
+                }
+
+                if (resultado <= 2)
+                        printf("%d\n", j);
+        }
+        return 0;
 
 
 }
